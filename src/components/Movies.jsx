79580c958595693734209ea886ace5c76a106747
Seba <sebase.pereyra@gmail.com>
2024-01-18 19:36:48 -0300

    Add styles and show as grid

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -2,10 +2,10 @@
 import withoutResults from '.././mocks/movies-list.json'
 
 function ListOfMovies({ movies }) {
-    return (<ul>
+    return (<ul className='movies'>
         {
             movies.map(movie => (
-                <li key={movie.id}>
+                <li className='movie' key={movie.id}>
                     <h3>{movie.title}</h3>
                     <p>{movie.year}</p>
                     <img src={movie.poster}></img>
