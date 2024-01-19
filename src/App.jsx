f48efe8825c1ecfdc2af1a920df7fd2e8ebf6dab
Seba <sebase.pereyra@gmail.com>
2024-01-19 01:00:05 -0300

    Setting up a debouncer to auto run search

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import { useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import './App.css'
 import { Movies } from './components/Movies'
 import { useMovies } from './hooks/useMovies';
+import debounce from 'just-debounce-it';
 
 function useSearch() {
   const [search, setSearch] = useState('');
@@ -37,6 +38,13 @@ function App() {
   const [sort, setSort] = useState(false);
   const { movies, getMovies, isLoading } = useMovies({ sort });
 
+  const debouncedGetMovies = useCallback(
+    debounce(search => {
+      getMovies({ search })
+    }, 500)
+    , []);
+
+
   const handleSubmit = (event) => {
     event.preventDefault();
     getMovies({ search });
@@ -46,6 +54,7 @@ function App() {
     const newNameFilter = event.target.value;
     if (newNameFilter.startsWith(' ')) return;
     setSearch(newNameFilter);
+    debouncedGetMovies(newNameFilter);
   }
 
   const handleSort = () => {
